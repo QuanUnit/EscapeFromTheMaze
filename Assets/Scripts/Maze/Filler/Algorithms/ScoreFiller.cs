c830d509eb938e012b1066cb5322820c6036a525
Egor <81716234+QuanUnit@users.noreply.github.com>
2021-10-22 20:29:12 +0300

    Change score prefab, picking score point sound

--- Assets/Scripts/Maze/Filler/Algorithms/ScoreFiller.cs
+++ Assets/Scripts/Maze/Filler/Algorithms/ScoreFiller.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public abstract class ScoreFiller: MazeFillingAlgorithm
 {
-    [SerializeField] protected ScorePoint _scorePointPrefab;
+    [SerializeField] protected GameObject _scorePointPrefab;
     
     protected void CreateScorePoints(List<MazeCell> branch, int startIndex, int length, Transform container)
     {
